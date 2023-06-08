7d7f9234f5ee264ea3a1012b9f47552a53cded6e
Arnav628 <135796735+Arnav628@users.noreply.github.com>
2023-06-08 07:11:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const moviesContainer = document.getElementById("moviesContainer");
+
+
+async function fetchMovies() {
+  try {
+    const response = await fetch("https://api.themoviedb.org/3/trending/all/day?api_key=3b8ff0ac16ba4ad7a704e3a8fcdaac38"); 
+    const data = await response.json();
+    const movies = data.results; 
+    
+  
+    movies.forEach((movie) => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+      
+      const poster = document.createElement("img");
+      poster.src ='http://image.tmdb.org/t/p/w500'+ movie.backdrop_path; 
+      poster.alt = movie.title||movie.name;
+      poster.vote=movie.vote_average; 
+      poster.classList.add("poster");
+      
+      const overlay = document.createElement("div");
+      overlay.classList.add("overlay");
+      
+      const title = document.createElement("h3");
+      title.classList.add("title");
+      title.textContent = movie.title||movie.name; 
+      const vote = document.createElement("h1");
+      vote.classList.add("title");
+      vote.textContent = movie.vote_average;
+      overlay.appendChild(title);
+      overlay.appendChild(vote);
+      card.appendChild(poster);
+      card.appendChild(overlay);
+      moviesContainer.appendChild(card);
+    });
+  } catch (error) {
+    console.log("Error fetching movies:", error);
+  }
+}
+
+
+fetchMovies();
